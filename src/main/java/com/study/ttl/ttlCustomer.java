048e680a12514889eb344a1813b7e2fea487abce
WorldBlueSky <ChenRuiQi21@163.com>
2022-09-18 01:41:30 +0800

    更新代码

--- src/main/java/com/study/ttl/ttlCustomer.java
+++ src/main/java/com/study/ttl/ttlCustomer.java
@@ -13,19 +13,19 @@ import java.util.Date;
 @Component
 public class ttlCustomer {
 
-//     // 监听 QA队列，对其中的消息进行 业务消费
-//    @RabbitListener(queues = "QA")
-//     public void qa(Message message){
-//     String msg = new String(message.getBody());
-//     log.info("{} 收到QA队列的消息是 {}",new Date().toString(),msg);
-//     }
-//
-//     // 监听 QB队列，对其中的消息进行 业务消费
-//    @RabbitListener(queues = "QB")
-//     public void qb(Message message){
-//     String msg = new String(message.getBody());
-//     log.info("{} 收到QB队列的消息是 {}",new Date().toString(),msg);
-//     }
+     // 监听 QA队列，对其中的消息进行 业务消费
+    @RabbitListener(queues = "QA")
+     public void qa(Message message){
+     String msg = new String(message.getBody());
+     log.info("{} 收到QA队列的消息是 {}",new Date().toString(),msg);
+     }
+
+     // 监听 QB队列，对其中的消息进行 业务消费
+    @RabbitListener(queues = "QB")
+     public void qb(Message message){
+     String msg = new String(message.getBody());
+     log.info("{} 收到QB队列的消息是 {}",new Date().toString(),msg);
+     }
 
     // 监听 死信 QD队列，对其中的消息进行 业务消费
     @RabbitListener(queues = "QD")
